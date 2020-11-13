0363e2a61671b3d2d3ab8b613c8bbb935955d0c6
Alexander Rohrig <me@alexanderrohrig.com>
2020-11-13 10:20:59 -0800

    Updated preferences

--- Shared/PreferencesView.swift
+++ Shared/PreferencesView.swift
@@ -8,13 +8,25 @@
 import SwiftUI
 
 struct PreferencesView: View {
+    
+    @Environment(\.openURL) var openURL
+    @State var showAckPop = false
+    
     var body: some View {
         Form {
             //Divider()
-            Button("Acknowledgements") {}
-            Button("Donate") {}
-            Button("Help With Translations") {}
-            Button("Report a Bug") {}
+            Button("Acknowledgements") { showAckPop.toggle() }.popover(isPresented: self.$showAckPop, arrowEdge: .bottom) {
+                VStack {
+                    Text("Data Provider")
+                    Divider()
+                    Link("The Covid Tracking Project", destination: URL(string: "https://covidtracking.com/")!)
+                    Text("No changes were made.")
+                    Link("CC 4.0 License", destination: URL(string: "https://creativecommons.org/licenses/by/4.0/")!)
+                }.padding()
+            }
+            Button("Donate") { openURL(URL(string: "https://www.buymeacoffee.com/BeAQzFbUP")!) }
+            Button("Help With Translations") { openURL(URL(string: "https://alexanderrohrig.com/translationhelp")!) }
+            Button("Report a Bug") { openURL(URL(string: "https://github.com/Nsfwood/sanlorenzo/issues")!) }
             Text("Made with ❤️")
         }.padding()
     }
--- Shared/sanlorenzoApp.swift
+++ Shared/sanlorenzoApp.swift
@@ -9,12 +9,21 @@ import SwiftUI
 
 @main
 struct sanlorenzoApp: App {
+    
+    @Environment(\.openURL) var openURL
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }.commands {
+            CommandGroup(after: CommandGroupPlacement.appInfo) {
+                Button("Check for Updates...") { openURL(URL(string: "https://github.com/Nsfwood/sanlorenzo/releases")!) }
+            }
         }
+        #if os(macOS)
         Settings {
             PreferencesView()//.frame(width: 200, height: 200)
         }
+        #endif
     }
 }

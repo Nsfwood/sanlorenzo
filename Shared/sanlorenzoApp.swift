a2fa85aa2b38fefbf96bcbc1fff840c0a643e067
Alexander Rohrig <me@alexanderrohrig.com>
2020-11-12 18:59:25 -0800

    Initial Commit

---
+++ Shared/sanlorenzoApp.swift
@@ -0,0 +1,17 @@
+//
+//  sanlorenzoApp.swift
+//  Shared
+//
+//  Created by Alexander Rohrig on 11/12/20.
+//
+
+import SwiftUI
+
+@main
+struct sanlorenzoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
